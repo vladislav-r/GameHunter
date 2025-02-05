e8d1fc1869449eaeb873c1c53dc5d51f87bf257f
Martomerz <vladchern9@gmail.com>
2025-02-06 01:50:00 +1000

    first commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+const { query } = require('express');
+const ApiError = require('../error/ApiError');
+
+class UserController {
+	async register(req, res) {}
+	async login(req, res) {}
+	async check(req, res, next) {
+		const { id } = req.query;
+		if (!id) {
+			return next(ApiError.badRequest('Не задан ID'));
+		}
+		res.json(id);
+	}
+}
+
+module.exports = new UserController();
