01d9a9aa3d6cfebd53c53b6edd7533d8f91eafe5
Martomerz <vladchern9@gmail.com>
2025-02-09 22:21:38 +1000

    Обновлен README.md

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,8 @@ app.use(errorHandler);
 
 const start = async () => {
 	try {
-		await sequelize.authenticate();
-		await sequelize.sync();
+		// await sequelize.authenticate();
+		// await sequelize.sync();
 		app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 	} catch (e) {
 		console.log(e);
