f18ff1bdfa7beeaefd3a461010327b758b9d5be1
Martomerz <vladchern9@gmail.com>
2025-02-10 12:23:21 +1000

    Обновлен README.md

--- backend/routes/gameRouter.js
+++ backend/routes/gameRouter.js
@@ -6,5 +6,5 @@ const gameController = require('../controllers/gameController');
 router.get('/', gameController.getGames);
 router.get('/:id', gameController.showGame);
 router.get('/:id/price', gameController.getGamePrices);
-
+// router.get('/categories', gameController.getCategories)
 module.exports = router;
